cdf41aaeba30b903f3b0677c0bd339b3ed58049b
indemio <indemio@gmail.com>
2020-03-17 22:15:15 +0800

    upd

--- parser.py
+++ parser.py
@@ -1,4 +1,18 @@
+import sqlite3
 from xml.etree import cElementTree as ET
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
 
 
 def parser(xmlFile):
@@ -8,11 +22,8 @@ def parser(xmlFile):
     for books in root:
         for book in books:
             parse_list[book.tag]=book.text
-    for key in parse_list:
-        print(parse_list[key])
-
-#test раз два
-
+        for key in parse_list:
+            print(parse_list[key])
 
 
 if __name__ == "__main__":
